213d5fbf317d64180c6716aaccc4a4388fc44aef
Abd Asis <id.abdasis@gmail.com>
2021-02-05 15:03:22 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Livewire\Index;
 use App\Http\Livewire\Posts\Show;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,3 +21,7 @@ Route::get('/{slug_post}', Show::class)->name('post.show');
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
+
+Route::get('/clear-cache', function () {
+    $exitCode = Artisan::call('cache:clear');
+});
